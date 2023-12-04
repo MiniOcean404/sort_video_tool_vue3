5f7cdedd264c12316b62b8bc244f4612603a2e8c
MiniOcean404 <1037306928@qq.com>
2023-12-04 20:27:36 +0800

    feat: prettier

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,41 @@
 <script setup lang="ts">
-import {ref,reactive} from 'vue'
-import html2canvas from 'html2canvas'
-import TimeIcon from '@/assets/image/time-icon.png'
+import { ref, reactive } from "vue"
+import html2canvas from "html2canvas"
+import TimeIcon from "@/assets/image/time-icon.png"
 
 const props = defineProps({
-    bgColor: {
-      type: String,
-      default: '#000000'
-    },
-    bgWidth: {
-      type: [Number, String],
-      default: '100%'
-    },
-    bgHeight: {
-      type: [Number, String],
-      default: '100%'
-    },
-    color: {
-      type: String,
-      default: 'rgba(255,255,255,.6)'
-    }
+  bgColor: {
+    type: String,
+    default: "#000000",
+  },
+  bgWidth: {
+    type: [Number, String],
+    default: "100%",
+  },
+  bgHeight: {
+    type: [Number, String],
+    default: "100%",
+  },
+  color: {
+    type: String,
+    default: "rgba(255,255,255,.6)",
+  },
 })
 
-const config =  reactive({
-  borderSize: '15px',
+const config = reactive({
+  borderSize: "15px",
 })
 
 const color = ref(randomColor())
-const showUrl = ref('')
+const showUrl = ref("")
 const cover = ref<HTMLElement>()
 
-
-function start (){
-  if(cover.value) {
+function start() {
+  if (cover.value) {
     html2canvas(cover.value, {
-      scale: window.devicePixelRatio
-    }).then(canvas => {
-      showUrl.value = canvas.toDataURL('image/png')
+      scale: window.devicePixelRatio,
+    }).then((canvas) => {
+      showUrl.value = canvas.toDataURL("image/png")
     })
   }
 }
@@ -69,7 +68,7 @@ function randomColor() {
 <style scope lang="scss">
 // v-bind: https://blog.csdn.net/weixin_52235488/article/details/126290046
 
-.cover-box{
+.cover-box {
   color: #fff;
   font-weight: 900;
 
@@ -81,12 +80,12 @@ function randomColor() {
   justify-content: center;
   align-items: center;
 
-  .center{
-   .title{
-    position: relative;
+  .center {
+    .title {
+      position: relative;
       font-size: 80px;
 
-      .font{
+      .font {
         position: relative;
         z-index: 1;
       }
@@ -95,28 +94,28 @@ function randomColor() {
         content: attr(data-storke);
         position: absolute;
         z-index: 0;
-        -webkit-text-stroke: v-bind('config.borderSize') #000;
-        text-stroke: v-bind('config.borderSize') #000;
+        -webkit-text-stroke: v-bind("config.borderSize") #000;
+        text-stroke: v-bind("config.borderSize") #000;
       }
     }
 
-  .time-box{
-    margin: 30px auto 0;
-    padding: 5px 10px;
-    width: max-content;
-    background-color:#000;
-    border-radius: 1000px;
+    .time-box {
+      margin: 30px auto 0;
+      padding: 5px 10px;
+      width: max-content;
+      background-color: #000;
+      border-radius: 1000px;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-    .icon{
-      display: inline-block;
-      width: 16px;
-      height: 16px;
+      .icon {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+      }
     }
-  }
   }
 }
 </style>
