243511b3916380410dfdbdc53062c25e43c2dce3
MiniOcean404 <1037306928@qq.com>
2023-12-06 00:37:03 +0800

    chore: vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { GieResolver } from "@giegie/resolver"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // 开发或生产环境服务的公共基础路径
   plugins: [
     vue(),
     Components({
@@ -24,6 +25,19 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  build: {
+    sourcemap: true, // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件。
+    assetsInlineLimit: 4096, // 图片转 base64 编码的阈值
+    // 代码压缩配置
+    terserOptions: {
+      // 生产环境移除 console
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   server: {
     port: 3000, //启动端口
@@ -31,5 +45,24 @@ export default defineConfig({
       host: "127.0.0.1",
       port: 3000,
     },
+  },
+  css: {
+    modules: {
+      scopeBehaviour: "local",
+    },
+    preprocessorOptions: {
+      scss: {
+        /* .scss全局预定义变量，引入多个文件 以;(分号分割)*/
+        // additionalData: `@import "./src/assets/css/global.scss";`,
+      },
+    },
+    // 可以查看 CSS 的源码
+    devSourcemap: true,
+  },
+  optimizeDeps: {
+    // 是否开启强制依赖预构建。node_modules 中的依赖模块构建过一次就会缓存在 node_modules/.vite/deps 文件夹下，下一次会直接使用缓存的文件。
+    // 而有时候我们想要修改依赖模块的代码，做一些测试或者打个补丁，这时候就要用到强制依赖预构建。
+    // 除了这个方法，我们还可以通过删除 .vite 文件夹或运行 npx vite --force 来强制进行依赖预构建。
+    force: false, // 强制进行依赖预构建
   },
 })
