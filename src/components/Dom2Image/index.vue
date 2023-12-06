b6f3d257a202585b87412b8a81424b81d098e04c
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-12-06 15:38:05 +0800

    feat: 动态扩展

--- src/components/Dom2Image/index.vue
+++ src/components/Dom2Image/index.vue
@@ -50,14 +50,16 @@ const download = (filename: string, url: string) => {
 <template>
   <div class="cover-box" ref="cover">
     <div :class="['show-area', { border: isShowBorder }]">
-      <div class="center">
-        <div class="title" data-storke="封面制作">
-          <span class="font">{{ props.title }}</span>
-        </div>
+      <div class="dyn-container">
+        <div class="center">
+          <div class="title" data-storke="封面制作">
+            <span class="font">{{ props.title }}</span>
+          </div>
 
-        <div class="time-box">
-          <img class="icon" :src="TimeIcon" alt="" />
-          <span>{{ props.minute }}分{{ props.seconds }}秒</span>
+          <div class="time-box">
+            <img class="icon" :src="TimeIcon" alt="" />
+            <span>{{ props.minute }}分{{ props.seconds }}秒</span>
+          </div>
         </div>
       </div>
     </div>
@@ -92,41 +94,53 @@ const download = (filename: string, url: string) => {
     justify-content: center;
     align-items: center;
 
-    .center {
-      .title {
-        position: relative;
-        font-size: 80px;
+    position: relative;
 
-        .font {
+    .dyn-container {
+      position: absolute;
+      inset: 0;
+      container-type: inline-size;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .center {
+        .title {
           position: relative;
-          z-index: 1;
+          font-size: 20cqw;
+
+          .font {
+            position: relative;
+            z-index: 1;
+          }
+
+          &::before {
+            content: attr(data-storke);
+            position: absolute;
+            z-index: 0;
+            -webkit-text-stroke: v-bind("config.borderSize") #000;
+            text-stroke: v-bind("config.borderSize") #000;
+          }
         }
 
-        &::before {
-          content: attr(data-storke);
-          position: absolute;
-          z-index: 0;
-          -webkit-text-stroke: v-bind("config.borderSize") #000;
-          text-stroke: v-bind("config.borderSize") #000;
-        }
-      }
+        .time-box {
+          margin: 7.5cqw auto 0;
+          padding: 2.5cqw 5cqw;
 
-      .time-box {
-        margin: 30px auto 0;
-        padding: 5px 10px;
+          background-color: #000;
+          width: max-content;
+          border-radius: 1000px;
 
-        background-color: #000;
-        width: max-content;
-        border-radius: 1000px;
+          display: flex;
+          align-items: center;
 
-        display: flex;
-        align-items: center;
-
-        .icon {
-          margin-right: 10px;
-          display: inline-block;
-          width: 16px;
-          height: 16px;
+          .icon {
+            margin-right: 5cqw;
+            display: inline-block;
+            width: 4cqw;
+            height: 4cqw;
+          }
         }
       }
     }
