ff88f6c741b10935f387547245827360232c46e0
MiniOcean404 <1037306928@qq.com>
2023-12-14 18:17:00 +0800

    feat: 动态路由

---
+++ src/router/dynRoute.ts
@@ -0,0 +1,28 @@
+import { RouteMeta, RouteRecordRaw } from "vue-router"
+
+export const routes: Array<RouteRecordRaw> = getRoutes()
+
+function getRoutes() {
+  const comp = import.meta.glob("/src/(pages|view)/**/index.vue")
+
+  const configs = import.meta.glob<RouteMeta>([`/src/(pages|view)/**/config.[tj]s`], {
+    // 获取导出的模块
+    eager: true,
+    // 直接获取 default 结果
+    import: "default",
+  })
+
+  return Object.entries(configs).map(([path, meta]) => {
+    const compPath = path.replace("config.ts", "index.vue")
+
+    path = path.replace("/src/pages", "").replace("/config.ts", "") || "/"
+    const name = path.split("/").join("-") || "index"
+
+    return {
+      path,
+      name,
+      meta,
+      component: comp[compPath],
+    }
+  })
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Index",
-    meta: {
-      title: "首页",
-      keepAlive: true,
-      requireAuth: true,
-    },
-    component: () => import("@/pages/index.vue"),
-  },
-];
+import { routes } from "@/router/dynRoute"
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
-export default router;
+})
+export default router
