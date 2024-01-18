f08af4733657b659f6bed58cba8b5cc2e831a9db
MiniOcean404 <1037306928@qq.com>
2024-01-18 21:20:09 +0800

    chore: vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -331,7 +331,7 @@ export default defineConfig((config) => {
     preview: {
       port: 5000, // 指定开发服务器端口
       strictPort: false, // 若端口已被占用则会直接退出
-      https: false, // 启用 TLS + HTTP/2
+      https: {}, // 启用 TLS + HTTP/2
       open: true, // 启动时自动在浏览器中打开应用程序
       proxy: {
         // 配置自定义代理规则
